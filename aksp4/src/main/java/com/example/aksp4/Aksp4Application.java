1c418bc08ce02de72cff7dd066349aa7550e023e
queerxdisasster <7germania7@gmail.com>
2023-10-29 21:03:50 +0300

    4

---
+++ aksp4/src/main/java/com/example/aksp4/Aksp4Application.java
@@ -0,0 +1,13 @@
+package com.example.aksp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aksp4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aksp4Application.class, args);
+    }
+
+}
---
+++ aksp4/src/test/java/com/example/aksp4/Aksp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aksp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aksp4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
